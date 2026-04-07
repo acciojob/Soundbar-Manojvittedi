ef119e12e8264bdd3dc535f1f55a6e86b231d03c
Manojvittedi <153159811+Manojvittedi@users.noreply.github.com>
2026-04-07 09:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let audio;
+
+function playSound(name) {
+    audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function stopSound() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
